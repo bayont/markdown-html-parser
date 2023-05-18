8db62995ea28b2f1c7e302988dd26f3037d24f5e
bayont <f.fetter02@gmail.com>
2023-05-19 01:20:40 +0200

    initial commit

---
+++ src/main/java/com/example/markdownhtmlparser/HelloController.java
@@ -0,0 +1,49 @@
+package com.example.markdownhtmlparser;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private Label inputLabel;
+
+    @FXML
+    private Label outputLabel;
+
+    @FXML
+    private TextArea inputTextArea;
+
+    @FXML
+    private TextArea outputTextArea;
+    
+    private ParserMode parserMode = ParserMode.HTML_TO_MARKDOWN;
+
+    @FXML
+    protected void onSwitchParserModeButtonClicked(ActionEvent event) {
+        parserMode = parserMode == ParserMode.HTML_TO_MARKDOWN ? ParserMode.MARKDOWN_TO_HTML : ParserMode.HTML_TO_MARKDOWN;
+        evaluateLabels();
+    }
+
+    @FXML
+    public void initialize() {
+        this.evaluateLabels();
+    }
+
+    private void evaluateLabels() {
+        if(parserMode == ParserMode.HTML_TO_MARKDOWN) {
+            inputLabel.setText("Input (HTML)");
+            outputLabel.setText("Output (Markdown)");
+            welcomeText.setText("HTML -> Markdown Parser");
+
+        } else if(parserMode == ParserMode.MARKDOWN_TO_HTML) {
+            inputLabel.setText("Input (Markdown)");
+            outputLabel.setText("Output (HTML)");
+            welcomeText.setText("Markdown -> HTML Parser");
+        }
+    }
+}
---
+++ src/main/java/com/example/markdownhtmlparser/ParserMode.java
@@ -0,0 +1,6 @@
+package com.example.markdownhtmlparser;
+
+public enum ParserMode {
+    HTML_TO_MARKDOWN,
+    MARKDOWN_TO_HTML
+}
